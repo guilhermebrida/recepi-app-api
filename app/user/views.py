1efde6929c3c5a4747706e054fb24fe2b8b96b9a
guilhermebrida <43304919+guilhermebrida@users.noreply.github.com>
2023-07-06 22:27:04 -0300

    correcao codigo

--- app/user/views.py
+++ app/user/views.py
@@ -3,6 +3,6 @@
 from rest_framework import generics
 from user.serializers import UserSerializer
 
-class CreateUuserView(generics.CreateAPIView):
+class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
