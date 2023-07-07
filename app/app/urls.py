c56dd88b67aaf1e6a4fed8f3c76c3f2ba0735041
guilhermebrida <43304919+guilhermebrida@users.noreply.github.com>
2023-07-06 22:18:55 -0300

    correcao codigo

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +28,7 @@ urlpatterns = [
         'api/docs/', 
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
+    ),
+    path('api/user/', include('user.urls'))
     
 ]
